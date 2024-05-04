6c0847fb1e95d53a5f5b7b6d2239391cece7d586
Olivereliasandersen <oland0229@oslomet.no>
2024-05-04 11:48:33 +0200

    ahd

---
+++ src/main/java/com/example/eksamen2023/Eksamen2023Application.java
@@ -0,0 +1,13 @@
+package com.example.eksamen2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamen2023Application.class, args);
+    }
+
+}
