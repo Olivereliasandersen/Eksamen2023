f2206d0814f5ea139a8bf5cb72b827b04a98c50f
Olivereliasandersen <oland0229@oslomet.no>
2024-05-04 13:39:37 +0200

    askdfnjkadsfna

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,43 @@
+function validatePhoneNumber(){
+    const phoneNumber = $("#phoneNumber").val();
+    const regexp = /^[0-9]{8}$/;
+    const ok = regexp.test(phoneNumber);
+
+    if (!ok){
+        $("#wrongPhoneNumber").html("Phone number should be 8 digits long");
+        return false;
+    } else {
+        $("#wrongPhoneNumber").html("");
+        return true;
+    }
+}
+function validateEmail(){
+    const email = $("#email").val();
+    const regexp = /^[A-Za-z0-9._+/-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2}$/;
+    const ok = regexp.test(email);
+
+    if (!ok){
+        $("#wrongEmail").html("You have to enter a valid email");
+        return false;
+    } else {
+        $("#wrongEmail").html("");
+        return true;
+    }
+}
+
+function sendForm(){
+    const citizen = {
+        firstname: $("#firstname").val(),
+        surname: $("#surname").val(),
+        DoB: $("#dateOfBirth").val(),
+        SSN: $("#SSN").val(),
+        phoneNumber: $("#phoneNumber").val(),
+        email: $("#email").val(),
+        city: $("#city").val(),
+        street: $("#street").val()
+    };
+
+    console.log(citizen)
+
+    if ()
+}
